d02a2f8eebe31b504a18ef744f985614ec525bda
priyas35 email = priyaharisubi@gmail.com <User1@LP-5CD9176LCF.HCLT.CORP.HCL.IN>
2019-12-23 20:21:56 +0530

    committed for viewmyorders

---
+++ src/main/java/com/store/stock/constant/AppConstant.java
@@ -0,0 +1,12 @@
+package com.store.stock.constant;
+
+public class AppConstant {
+	
+       private AppConstant() {
+    	   
+	}
+       
+       public static final String ORDER_NOT_FOUND = "Order Not Placed";
+       public static final String USER_NOT_FOUND = "User Not Found";
+
+}
--- src/main/java/com/store/stock/controller/OrderController.java
+++ src/main/java/com/store/stock/controller/OrderController.java
@@ -1,5 +1,33 @@
 package com.store.stock.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.store.stock.constant.AppConstant;
+import com.store.stock.entity.Order;
+import com.store.stock.exception.OrderNotFoundException;
+import com.store.stock.exception.UserNotFoundException;
+import com.store.stock.service.OrderService;
+
 public class OrderController {
+	
+	@Autowired
+	OrderService orderService;
+
+	@GetMapping("/{userId}")
+	public ResponseEntity<List<Order>> viewMyOrder(@PathVariable("userId") Integer userId)
+			throws OrderNotFoundException, UserNotFoundException {
+		if (userId == null) {
+			throw new UserNotFoundException(AppConstant.USER_NOT_FOUND);
+		} else {
+			return new ResponseEntity<List<Order>>(orderService.viewMyOrder(userId), HttpStatus.OK);
+		}
+
+	}
 
 }
